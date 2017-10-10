59aa417cd242284b6e672913edfdb945e668137c
Taynara Sene <tay.sene@gmail.com>
2017-10-10 17:59:35 -0300

    commit initial :tada:

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Node-GraphQL
+
+API utilizando GraphQL
+
+Taynara Sene - 538515
+Cintia Silman - 
+
+## Install
+
+```
+npm install
+
+```
+
+## Open
+
+```
+npm rum start
+```
---
+++ index.js
@@ -0,0 +1,10 @@
+const graphql = require('graphql')
+const graphqlHTTP = require('express-graphql')
+const express = require('express')
+const users = require('./schema')
+const app = express()
+app.use('/user', graphqlHTTP({schema:users, pretty: true}))
+app.listen(3000, function () {
+  console.log('Server on. http://localhost:3000/ ')
+  console.log('ex. http://localhost:3000/user?query={user(id:2){id,name,age,knowledge{language,frameworks}}} ')
+})
